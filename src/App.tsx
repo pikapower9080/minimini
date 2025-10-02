db0c460e106f8248c0b7d711bad808398eb4cb13
pikapower9080 <pikapower9080@gmail.com>
2025-10-02 13:58:22 -0400

    feat: welcome modal

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,12 @@ import { useEffect, useState } from 'react'
 import './App.css'
 import type { MiniCrossword } from './lib/types'
 import Mini from './Components/Mini';
+import Modal from 'react-responsive-modal';
 
 function App() {
   const [data, setData] = useState<MiniCrossword | null>(null);
+  const [modalOpen, setModalOpen] = useState(true);
+  // const [paused, setPaused] = useState(false);
 
   useEffect(() => {
     const cached = localStorage.getItem('mini-cache');
@@ -24,8 +27,20 @@ function App() {
 
   return (
     <>
-      {/* <h2>Minimini</h2> */}
-      {data ? <Mini data={data} /> : <p>Loading...</p>}
+      {data && <Modal open={modalOpen} onClose={() => {}} showCloseIcon={false} center classNames={{ modal: 'welcome-modal' }}>
+        <h2>Welcome to minimini</h2>
+        <h4>{new Date(data.publicationDate).toLocaleDateString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }).replace(/\b(\d{1,2})\b/, (match) => {
+          const suffix = ['th', 'st', 'nd', 'rd'];
+          const day = parseInt(match, 10);
+          const value = day % 100;
+          return day + (suffix[(value - 20) % 10] || suffix[value] || suffix[0]);
+        })}</h4>
+        <h4>by {data.constructors.join(", ")}</h4>
+        <button onClick={() => {
+          setModalOpen(false);
+        }}>Start Solving</button>
+      </Modal>}
+      {data && !modalOpen ? <Mini data={data} /> : <p>Loading...</p>}
     </>
   )
 }
