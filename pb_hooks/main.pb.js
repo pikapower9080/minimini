901224c2ebabe0b2b6599b05bbba9654167fac74
pika <pikapower9080@gmail.com>
2025-11-29 19:09:53 -0500

    fix: always show the mini for the server's current date

--- pb_hooks/main.pb.js
+++ pb_hooks/main.pb.js
@@ -1,20 +1,33 @@
 /// <reference path="../pb_data/types.d.ts" />
 
 routerAdd("GET", "/api/today", (e) => {
-  try {
-    const res = $http.send({
-      url: "https://www.nytimes.com/svc/crosswords/v6/puzzle/mini.json",
-      method: "GET",
-      body: "",
-      headers: {},
-      timeout: 120
-    });
+  function getTodayDateString() {
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, "0");
+    const day = String(today.getDate()).padStart(2, "0");
+    return `${year}-${month}-${day}`;
+  }
 
-    const data = res.json;
-    return e.json(200, data);
+  try {
+    const record = $app.findFirstRecordByData("archive", "publicationDate", getTodayDateString());
+    return e.json(200, record.get("mini"));
   } catch (err) {
-    console.error(err);
-    return e.json(500, { error: "Failed to fetch data" });
+    try {
+      const res = $http.send({
+        url: "https://www.nytimes.com/svc/crosswords/v6/puzzle/mini.json",
+        method: "GET",
+        body: "",
+        headers: {},
+        timeout: 120
+      });
+
+      const data = res.json;
+      return e.json(200, data);
+    } catch (err) {
+      console.error(err);
+      return e.json(500, { error: "Failed to fetch data" });
+    }
   }
 });
 
