cc30e57a0f1a93deb78bd329c653ce8e40be9982
pika <pikapower9080@gmail.com>
2025-12-07 13:29:49 -0500

    chore: remove account signin/out buttons from the puzzle menu

--- src/routes/mini/Components/PuzzleMenu.tsx
+++ src/routes/mini/Components/PuzzleMenu.tsx
@@ -1,11 +1,10 @@
 import { useContext, type RefObject } from "react";
 import { GlobalState } from "../../../lib/GlobalState";
-import { Menu, MenuDivider, MenuHeader, MenuItem } from "@szhsin/react-menu";
-import localforage from "localforage";
+import { Menu, MenuDivider, MenuItem } from "@szhsin/react-menu";
 import { pb } from "../../../main";
 import posthog from "posthog-js";
 import type { MiniCrossword } from "../../../lib/types";
-import { LogInIcon, LogOutIcon, MenuIcon, RotateCcwIcon, TrophyIcon } from "lucide-react";
+import { DoorOpenIcon, MenuIcon, RotateCcwIcon, TrophyIcon } from "lucide-react";
 import { MiniState } from "../state";
 
 export default function PuzzleMenu({
@@ -28,17 +27,6 @@ export default function PuzzleMenu({
     <Menu portal transition align="end" menuButton={<MenuIcon />}>
       {user ? (
         <>
-          <MenuHeader>{user.username}</MenuHeader>
-          <MenuItem
-            onClick={() => {
-              pb.authStore.clear();
-              localforage.clear().then(() => {
-                window.location.reload();
-              });
-            }}
-          >
-            <LogOutIcon /> Sign out
-          </MenuItem>
           <MenuItem
             onClick={() => {
               setPuzzleModalState("leaderboard");
@@ -51,14 +39,16 @@ export default function PuzzleMenu({
           <MenuDivider />
         </>
       ) : (
-        <MenuItem
-          onClick={() => {
-            setModalState("sign-in");
-          }}
-        >
-          <LogInIcon /> Sign in
-        </MenuItem>
+        ""
       )}
+      <MenuItem
+        onClick={() => {
+          location.reload();
+        }}
+      >
+        <DoorOpenIcon />
+        Exit
+      </MenuItem>
       <MenuItem
         onClick={() => {
           clearLocalPuzzleData().then(() => {
